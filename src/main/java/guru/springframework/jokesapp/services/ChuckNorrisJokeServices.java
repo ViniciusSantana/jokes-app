2770a2e2b0a4e0ae12a8570a3472d68bcda6ec1b
Vinicius <vinicius.santana@trustly.com>
2020-03-15 14:43:49 -0300

    Use configuration class

--- src/main/java/guru/springframework/jokesapp/services/ChuckNorrisJokeServices.java
+++ src/main/java/guru/springframework/jokesapp/services/ChuckNorrisJokeServices.java
@@ -2,14 +2,16 @@ package guru.springframework.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.stereotype.Service;
-
-import java.lang.invoke.VarHandle;
 @Service
 public class ChuckNorrisJokeServices implements JokeServices {
-    private final ChuckNorrisQuotes quotes = new ChuckNorrisQuotes ();
+    private final ChuckNorrisQuotes quotes;
+
+    public ChuckNorrisJokeServices(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
+    }
 
     @Override
-    public String getJoke(){
+    public String getJoke() {
         return quotes.getRandomQuote();
     }
 }
